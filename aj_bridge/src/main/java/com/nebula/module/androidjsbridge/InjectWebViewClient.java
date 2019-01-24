068c4b832927cb1cc910f20b46a7eca2a3f16379
nebulae-pan <2595612295@qq.com>
2019-01-24 23:46:25 +0800

    add annotations for jsInvoke handle process

--- aj_bridge/src/main/java/com/nebula/module/androidjsbridge/InjectWebViewClient.java
+++ aj_bridge/src/main/java/com/nebula/module/androidjsbridge/InjectWebViewClient.java
@@ -5,10 +5,29 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class InjectWebViewClient extends WebViewClient {
+    public InjectWebViewClient(WebView webView){
+        webView.addJavascriptInterface(new JsInvokeHandler(),"native");
+    }
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
-        String injectString = "";
+        String injectString = "window.callNative = {};\n" +
+                "window.nativeCall = {};\n" +
+                "\n" +
+                "var count = 0;\n" +
+                "var callbackSet = {};\n" +
+                "\n" +
+                "window.callNative = function(method, params, callback){\n" +
+                "\tif(callback){\n" +
+                "\t\tcount++;\n" +
+                "\t\tcallbackSet[count] = callback;\n" +
+                "\t}\n" +
+                "\twindow.native.invokeDispatcher(method, JSON.stringify(params));\n" +
+                "};\n" +
+                "\n" +
+                "window.nativeCall = function(params){\n" +
+                "\t\n" +
+                "};";
         System.out.println(injectString.replaceAll("\n",""));
         view.loadUrl("javascript:" + injectString.replaceAll("\n", ""));
     }
--- app/src/main/java/com/nebula/module/androidjsbridge/demo/MainActivity.java
+++ app/src/main/java/com/nebula/module/androidjsbridge/demo/MainActivity.java
@@ -7,6 +7,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import com.nebula.module.androidjsbridge.InjectWebViewClient;
+import com.nebula.module.androidjsbridge.JsParam;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new InjectWebViewClient());
-        webView.setWebChromeClient(new WebChromeClient(){
+        webView.setWebViewClient(new InjectWebViewClient(webView));
+
+        webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
                 System.out.println("webView:" + consoleMessage.message());
                 return super.onConsoleMessage(consoleMessage);
             }
         });
-//        webView.loadUrl("javascript:window.callNative.call = function(){};");
         webView.loadUrl("file:///android_asset/test.html");
+    }
+
+    public void scanCode(@JsParam("name") String name) {
+
     }
 }
