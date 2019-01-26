0c5684e0fa3a7f4277cb098c9270b79acb04112f
nebulae-pan <2595612295@qq.com>
2019-01-27 00:27:19 +0800

    handle native callee

--- aj_bridge/src/main/java/com/nebula/module/androidjsbridge/JsInvokeHandler.java
+++ aj_bridge/src/main/java/com/nebula/module/androidjsbridge/JsInvokeHandler.java
@@ -1,28 +1,70 @@
 package com.nebula.module.androidjsbridge;
 
 import android.util.Log;
+import android.util.Pair;
 import android.webkit.JavascriptInterface;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class JsInvokeHandler {
-    Map<String, List<String>> jsMethodSet = new HashMap<>();
-    List<String> paramKeyList;
-    JsInvokeHandler(Object handleClass){
-        Method[] methods = handleClass.getClass().getMethods();
-        for(Method method:methods){
-            if (method.getAnnotation(JsInvoke.class) != null) {
+    private Map<String, Pair<Method, List<String>>> mNativeDisposeSet = new HashMap<>();
+    private Object mTarget;
+
+    JsInvokeHandler(Object handleClass) {
+        mTarget = handleClass;
+        Method[] methods = handleClass.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            Annotation methodAnnotation = method.getAnnotation(JsInvoke.class);
+            if (methodAnnotation != null) {
+                List<String> paramKeyList = new ArrayList<>();
                 Annotation[][] paramAnnotations = method.getParameterAnnotations();
+                for (Annotation[] annotations : paramAnnotations) {
+                    for (Annotation annotation : annotations) {
+                        if (annotation.annotationType().equals(JsParam.class)) {
+                            paramKeyList.add(((JsParam) annotation).value());
+                        }
+                    }
+                }
+                mNativeDisposeSet.put(((JsInvoke) methodAnnotation).value(), new Pair<>(method, paramKeyList));
             }
+        }
+        for (String key : mNativeDisposeSet.keySet()) {
+            Log.e("jsHandleMethod", key + ":" + Arrays.toString(mNativeDisposeSet.get(key).second.toArray()));
         }
     }
 
     @JavascriptInterface
-    public void invokeDispatcher(String method, String params){
+    public void invokeDispatcher(String method, String params) {
+        Pair<Method,List<String>> pair = mNativeDisposeSet.get(method);
+        Method nativeCallee = pair.first;
+        List<String> paramKeyList = pair.second;
+        List<String> paramList = new ArrayList<>();
+        try {
+            Log.e("tag", params);
+            JSONObject j = new JSONObject("{\"name\":\"zhang\",\"gender\":\"male\"}");
+            JSONObject jsonObject = new JSONObject(params);
+            for (String key : paramKeyList) {
+                paramList.add(jsonObject.getString(key));
+            }
+            nativeCallee.invoke(mTarget, paramList.toArray());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         Log.e("tag:", method + ":" + params);
     }
 }
--- app/src/main/java/com/nebula/module/androidjsbridge/demo/MainActivity.java
+++ app/src/main/java/com/nebula/module/androidjsbridge/demo/MainActivity.java
@@ -2,11 +2,13 @@ package com.nebula.module.androidjsbridge.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import com.nebula.module.androidjsbridge.InjectWebViewClient;
+import com.nebula.module.androidjsbridge.JsInvoke;
 import com.nebula.module.androidjsbridge.JsParam;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new InjectWebViewClient(webView));
+        webView.setWebViewClient(new InjectWebViewClient(webView, this));
 
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
@@ -29,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         webView.loadUrl("file:///android_asset/test.html");
     }
 
+    @JsInvoke("nativeScan")
     public void scanCode(@JsParam("name") String name) {
-
+        Log.e("tag", "name = " + name);
     }
 }
